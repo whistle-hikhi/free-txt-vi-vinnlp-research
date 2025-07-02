73af08730b97b695d32e41223877ad0371b81cf1
Hiro Nguyen <nguyenhuyhung192002@gmail.com>
2025-07-02 14:06:07 +0700

    update word-sum

--- api/internal/controller/free_txt.py
+++ api/internal/controller/free_txt.py
@@ -3,12 +3,19 @@ from fastapi import Request
 from loguru import logger
 from core.settings import settings
 from internal.common.schemas.free_txt import SummarizationRequest, SummarizationResponse
-from internal.services.summarisation import summarizer_summa
+from internal.services.summarisation import Summarizer
 
 
 class FreeTxtController:
+    summarizer: Summarizer
+
+    def __init__(self):
+        self.summarizer = Summarizer()
+
     async def summarization(
         self, sum_request: SummarizationRequest
     ) -> SummarizationResponse:
-        text = await summarizer_summa(sum_request.text, sum_request.ratio)
+        text = await self.summarizer.summarizer_summa(
+            sum_request.text, sum_request.ratio
+        )
         return SummarizationResponse(summarize_text=text)
--- api/internal/services/summarisation.py
+++ api/internal/services/summarisation.py
@@ -2,25 +2,27 @@ from nltk import sent_tokenize
 from summa.summarizer import summarize as summa_summarizer
 
 
-async def summarizer_summa(input_text, chosen_ratio, lang="vi"):
+class Summarizer:
 
-    # Convert input_text to a string if it isn't already
-    if not isinstance(input_text, str):
-        input_text = " ".join(map(str, input_text))
+    async def summarizer_summa(self, input_text, chosen_ratio):
 
-    # Ensure the chosen_ratio is at least 0.1
-    chosen_ratio = max(chosen_ratio, 0.1)
+        # Convert input_text to a string if it isn't already
+        if not isinstance(input_text, str):
+            input_text = " ".join(map(str, input_text))
 
-    # Get the summary using the text_rank_summarize function
-    summary = summa_summarizer(input_text, chosen_ratio)
+        # Ensure the chosen_ratio is at least 0.1
+        chosen_ratio = max(chosen_ratio, 0.1)
 
-    # Tokenize input_text to sentences
-    sentences = sent_tokenize(input_text)
+        # Get the summary using the text_rank_summarize function
+        summary = summa_summarizer(input_text, chosen_ratio)
 
-    # Return the summary or fallback if the summary is empty
-    if summary:
-        return summary
-    elif len(sentences) > 1:
-        return sentences[0]
-    else:
-        return "Unable to summarize the input text."
+        # Tokenize input_text to sentences
+        sentences = sent_tokenize(input_text)
+
+        # Return the summary or fallback if the summary is empty
+        if summary:
+            return summary
+        elif len(sentences) > 1:
+            return sentences[0]
+        else:
+            return "Unable to summarize the input text."
