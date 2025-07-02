1967360a29d4b80a4ab742f5dae1a53e29587a4a
ai.server <ai.server@techvify.com.vn>
2025-07-02 10:57:03 +0700

    update api route

---
+++ api/cmd/main.py
@@ -0,0 +1,60 @@
+import asyncio
+from typing import Callable
+
+from fastapi import FastAPI
+from loguru import logger
+from starlette.middleware.cors import CORSMiddleware
+import uvicorn
+
+from core.settings import settings
+from tools.uts_scheduler import scheduler
+from internal.handler.free_txt import FreeTxtHandler
+from internal.controller.free_txt import FreeTxtController
+from internal.routes.free_txt import FreeTxtRoute
+
+
+class App:
+    application: FastAPI
+
+    def on_init_app(self) -> Callable:
+        async def start_app() -> None:
+            # Controller
+            free_txt_controller = FreeTxtController()
+
+            free_txt_handler = FreeTxtHandler(free_txt_controller)
+
+            free_txt_router = FreeTxtRoute(free_txt_handler)
+
+            prefix = "/api/v1"
+            self.application.include_router(
+                free_txt_router.router, prefix=prefix + "/free_txt", tags=["Free-txt"]
+            )
+
+            scheduler.start()
+
+        return start_app
+
+    def on_terminate_app(self) -> Callable:
+        @logger.catch
+        async def stop_app() -> None:
+            pass
+
+        return stop_app
+
+    def __init__(self):
+        self.application = FastAPI(**settings.fastapi_kwargs)
+        self.application.add_middleware(
+            CORSMiddleware,
+            allow_origins=settings.allowed_hosts,
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+        self.application.add_event_handler("startup", self.on_init_app())
+        self.application.add_event_handler("shutdown", self.on_terminate_app())
+
+
+app = App().application
+
+if __name__ == "__main__":
+    uvicorn.run("cmd.main:app", host="0.0.0.0", port=8080, reload=True)
---
+++ api/internal/routes/free_txt.py
@@ -0,0 +1,74 @@
+from fastapi import APIRouter
+
+from internal.common.schemas.free_txt import (
+    MeaningAnalysisResponse,
+    SentimentChartResponse,
+    SummarizationResponse,
+    WordCloudResponse,
+    WordTreeResponse,
+    WordUseRelationshipsResponse,
+)
+from internal.handler.free_txt import FreeTxtHandler
+
+
+class UserRoute:
+    router: APIRouter
+    handler: FreeTxtHandler
+
+    def __init__(self, handler: FreeTxtHandler):
+        self.router = APIRouter()
+        self.handler = handler
+
+        self.router.add_api_route(
+            path="/meaning_analysis",
+            endpoint=self.handler.meaning_analysis,
+            methods=["POST"],
+            response_model=MeaningAnalysisResponse,
+            summary="Meaning Analysis",
+            description="Analysis the meaning of sentences",
+        )
+
+        self.router.add_api_route(
+            path="/sentiment_chart",
+            endpoint=self.handler.sentiment_chart,
+            methods=["POST"],
+            response_model=SentimentChartResponse,
+            summary="Sentiment Chart",
+            description="Create chart to view sentiment analysis",
+        )
+
+        self.router.add_api_route(
+            path="/summarization",
+            endpoint=self.handler.summarization,
+            methods=["POST"],
+            response_model=SummarizationResponse,
+            summary="Summarization",
+            description="Sumarize the text input",
+        )
+
+        self.router.add_api_route(
+            path="/word_cloud",
+            endpoint=self.handler.word_cloud,
+            methods=["POST"],
+            response_model=WordCloudResponse,
+            summary="Word Cloud",
+            description="Create Word Cloud",
+        )
+
+        self.router.add_api_route(
+            path="/word_tree",
+            endpoint=self.handler.word_tree,
+            methods=["POST"],
+            response_model=WordTreeResponse,
+            summary="Word Tree",
+            description="Create Word Tree",
+        )
+
+        self.router.add_api_route(
+            path="/word_use_relationships",
+            endpoint=self.handler.word_use_relationships,
+            methods=["POST"],
+            response_model=WordUseRelationshipsResponse,
+            summary="Word Use and Relationships",
+            description="Create Word Use and Relationships",
+        )
