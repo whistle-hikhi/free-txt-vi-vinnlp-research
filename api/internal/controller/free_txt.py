50489b2679a0e97414891d1de5868d871d56b07d
Hiro Nguyen <nguyenhuyhung192002@gmail.com>
2025-07-07 09:35:52 +0700

    feat/wordtree create

--- api/internal/controller/free_txt.py
+++ api/internal/controller/free_txt.py
@@ -2,18 +2,34 @@ from datetime import datetime, timedelta
 from fastapi import Request
 from loguru import logger
 from core.settings import settings
-from internal.common.schemas.free_txt import SummarizationRequest, SummarizationResponse
+from internal.common.schemas.free_txt import (
+    SummarizationRequest,
+    SummarizationResponse,
+    WordTreeRequest,
+    WordTreeResponse,
+)
 from internal.services.summarisation import Summarizer
+from internal.services.word_tree import WordTree
 
 
 class FreeTxtController:
     summarizer: Summarizer
+    wordtree: WordTree
 
     def __init__(self):
         self.summarizer = Summarizer()
+        self.wordtree = WordTree()
 
     async def summarization(
         self, sum_request: SummarizationRequest
     ) -> SummarizationResponse:
         text = await self.summarizer.sum_qwen(sum_request.text, sum_request.ratio)
         return SummarizationResponse(summarize_text=text)
+
+    async def gen_wordtree(self, wordtree_request: WordTreeRequest) -> WordTreeResponse:
+        tree = await self.wordtree.build_word_tree(
+            wordtree_request.text, wordtree_request.keyword
+        )
+        return WordTreeResponse(
+            word=tree["word"], left=tree["left"], right=tree["right"]
+        )
